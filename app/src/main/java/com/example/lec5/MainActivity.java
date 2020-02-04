b0e955b4934fc584a4464b5bd036c49c517a0cd1
300311853 <singhn24@student.douglascollege.ca>
2020-02-04 12:18:20 -0800

    complete

--- app/src/main/java/com/example/lec5/MainActivity.java
+++ app/src/main/java/com/example/lec5/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lec5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -41,6 +45,27 @@ public class MainActivity extends AppCompatActivity {
          MyCustomAdapter myAdapter =new MyCustomAdapter(this,attrList,attrPics);
 
          ListViewItems.setAdapter(myAdapter);
+
+         ListViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 switch(position)
+                 {
+                     case 0:
+                         startActivity(new Intent(Intent.ACTION_VIEW,
+                                 Uri.parse("https://www.themagnificentmile.com/")));
+                         break;
+
+                     case 1:
+                         startActivity(new Intent(MainActivity.this,NavyPierActivity.class));
+                         break;
+
+                     case 2:
+                         startActivity(new Intent(MainActivity.this,ArtInstActivity.class));
+                         break;
+                 }
+             }
+         });
 
     }
 }
